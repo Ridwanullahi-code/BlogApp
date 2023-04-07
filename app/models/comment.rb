44b0a8ff072498d8d9dc76fccc79a1deef08c00a
ridwanullahi <ridwanullahiajayi01@gmail.com.com>
2023-04-07 07:51:24 +0100

    add linters file

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,9 @@
 class Comment < ApplicationRecord
-	belongs_to :author, class_name: 'User'
-	belongs_to :post 
-	after_save :update_comments_count
+  belongs_to :author, class_name: 'User'
+  belongs_to :post
+  after_save :update_comments_count
 
-	def update_comments_count
-		post.increment!(:comments_counter)
-	end
+  def update_comments_count
+    post.increment!(:comments_counter)
+  end
 end
--- db/migrate/20230405164401_add_reference.rb
+++ db/migrate/20230405164401_add_reference.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 class AddReference < ActiveRecord::Migration[7.0]
   def change
-    add_reference :comments, :author, null:false, foreign_key: {to_table: :users}
-    add_reference :comments, :post, null:false, foreign_key:true
-    add_reference :posts, :author, null:false, foreign_key:{to_table: :users}
-    add_reference :likes, :author, null:false, foreign_key:{to_table: :users}
-    add_reference :likes, :post, null:false, foreign_key:true
+    add_reference :comments, :author, null: false, foreign_key: { to_table: :users }
+    add_reference :comments, :post, null: false, foreign_key: true
+    add_reference :posts, :author, null: false, foreign_key: { to_table: :users }
+    add_reference :likes, :author, null: false, foreign_key: { to_table: :users }
+    add_reference :likes, :post, null: false, foreign_key: true
   end
 end
