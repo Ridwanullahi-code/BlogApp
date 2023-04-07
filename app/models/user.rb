73cc3fd474d9ad70e68e43caf0a559fdf35558ba
ridwanullahi <ridwanullahiajayi01@gmail.com.com>
2023-04-07 12:18:52 +0100

    writing test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
-  has_many :comments, foreign_key: 'author_id'
-  has_many :posts, foreign_key: 'author_id'
-  has_many :likes, foreign_key: 'author_id'
-  after_save :most_recent_post
+  validates :name, presence: true
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  has_many :comments, foreign_key: 'author_id', dependent: :destroy
+  has_many :posts, foreign_key: 'author_id', dependent: :destroy
+  has_many :likes, foreign_key: 'author_id', dependent: :destroy
+  after_save :most_recent_posts
 
-  private
-
-  def most_recent_post
+  def most_recent_posts
     posts.order(created_at: :desc).includes(:user).limit(3)
   end
 end
