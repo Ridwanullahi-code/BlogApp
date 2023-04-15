24eabd523ea99ed7af499385af88ba7b43511bbf
ridwanullahi <ridwanullahiajayi01@gmail.com.com>
2023-04-15 19:34:56 +0100

    late

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,5 +7,21 @@ class PostsController < ApplicationController
   def show
     @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
+    @comment = Comment.new
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(author_id: params[:post][:user_id], title: params[:post][:title], text: params[:post][:text].gsub(/\r\n?/, '<br>'))
+
+    if @post.save
+      redirect_to user_path(params[:post][:user_id]), notice: 'Post was successfully created.'
+    else
+      render :new
+      flash.alert = 'Error!, Post not added!'
+    end
   end
 end
