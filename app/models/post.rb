566f55130530d23379a8e0b2501285346e8d3454
ridwanullahi <ridwanullahiajayi01@gmail.com.com>
2023-06-07 09:31:17 +0100

    last change

--- app/models/post.rb
+++
@@ -1,5 +0,0 @@
-class Post < ApplicationRecord
-	belongs_to :user
-	has_many :comments
-	has_many :likes
-end
--- app/models/user.rb
+++
@@ -1,5 +0,0 @@
-class User < ApplicationRecord
-  has_many :comments, :dependent => :destroy
-	has_many :posts, :dependent => :destroy
-	has_many :likes, :dependent => :destroy
-end
